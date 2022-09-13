a4db7f0045287417959fb72f91b600701382ecdc
Kent Theo Fourie <42357713+GundamDweeb@users.noreply.github.com>
2022-09-13 10:16:44 +0200

    fix & performance upgrade

--- src/mappings/inventoryStaking.ts
+++ src/mappings/inventoryStaking.ts
@@ -1,61 +1,77 @@
 import { Address, BigInt, dataSource } from "@graphprotocol/graph-ts";
-import { XTokenCreated, Deposit, Withdraw, NFTXInventoryStaking } from "../../generated/NFTXInventoryStaking/NFTXInventoryStaking";
+import {
+  XTokenCreated,
+  Deposit,
+  Withdraw,
+  NFTXInventoryStaking,
+} from "../../generated/NFTXInventoryStaking/NFTXInventoryStaking";
 import { VaultAsset } from "../../generated/schema";
-import { NFTXVaultFactoryUpgradeable } from "../../generated/templates/Token/NFTXVaultFactoryUpgradeable";
-import { ADDRESS_ZERO, NFTX_INVENTORY_STAKING, NFTX_VAULT_FACTORY } from "./utils/constants";
+import {
+  ADDRESS_ZERO,
+  NFTX_INVENTORY_STAKING,
+} from "./utils/constants";
 import { fetchAccount } from "./utils/fetch/account";
 import { fetchVaultAsset } from "./utils/fetch/vaultAsset";
-import { createTokenAndAssignAssetInfo } from "./utils/vaultIdAssignment";
+import {
+  createTokenAndAssignAssetInfo,
+  getVaultFromID,
+} from "./utils/vaultIdAssignment";
 
 export function handleXTokenCreated(event: XTokenCreated): void {
-  createTokenAndAssignAssetInfo(event.params.xToken, event.params.vaultId, "xToken");
+  createTokenAndAssignAssetInfo(
+    event.params.xToken,
+    event.params.vaultId,
+    "xToken"
+  );
 }
 
 export function handleXTokenDeposited(event: Deposit): void {
   const network: string = dataSource.network();
   const vaultId = event.params.vaultId;
 
-  let vaultAddress = getVaultAddress(vaultId, network);
-  let xTokenShareValue = getXTokenShareValue(vaultId, network);
-  
-  let account = fetchAccount(vaultAddress);
-  let vaultAsset = VaultAsset.load(vaultAddress.toHex());
-  if (!vaultAsset) {
-    vaultAsset = fetchVaultAsset(account.id);
+  let vault = getVaultFromID(vaultId);
+  if (vault != null) {
+    let vaultAddress = vault.address;
+    let xTokenShareValue = getXTokenShareValue(vaultId, network);
+
+    let account = fetchAccount(Address.fromBytes(vaultAddress));
+    let vaultAsset = VaultAsset.load(vaultAddress.toHex());
+    if (!vaultAsset) {
+      vaultAsset = fetchVaultAsset(account.id);
+    }
+    vaultAsset.xTokenShareValue = xTokenShareValue;
+    vaultAsset.save();
   }
-  vaultAsset.xTokenShareValue = xTokenShareValue;
-  vaultAsset.save();
 }
 
 export function handleXTokenWithdrawn(event: Withdraw): void {
   const network: string = dataSource.network();
   const vaultId = event.params.vaultId;
 
-  let vaultAddress = getVaultAddress(vaultId, network);
-  let xTokenShareValue = getXTokenShareValue(vaultId, network);
-  if (vaultAddress == ADDRESS_ZERO || xTokenShareValue == BigInt.fromI32(0)) return;
+  let vault = getVaultFromID(vaultId);
+  if (vault != null) {
+    let vaultAddress = vault.address;
+    let xTokenShareValue = getXTokenShareValue(vaultId, network);
+    if (vaultAddress == ADDRESS_ZERO || xTokenShareValue == BigInt.fromI32(0))
+      return;
 
-  let account = fetchAccount(vaultAddress);
-  let vaultAsset = VaultAsset.load(vaultAddress.toHex());
-  if (!vaultAsset) {
-    vaultAsset = fetchVaultAsset(account.id);
+    let account = fetchAccount(Address.fromBytes(vaultAddress));
+    let vaultAsset = VaultAsset.load(vaultAddress.toHex());
+    if (!vaultAsset) {
+      vaultAsset = fetchVaultAsset(account.id);
+    }
+    vaultAsset.xTokenShareValue = xTokenShareValue;
+    vaultAsset.save();
   }
-  vaultAsset.xTokenShareValue = xTokenShareValue;
-  vaultAsset.save();
-}
-
-function getVaultAddress(vaultId: BigInt, network: string): Address {
-  const nftxVaultFactory = NFTXVaultFactoryUpgradeable.bind(
-    NFTX_VAULT_FACTORY(network)
-  );
-  let vaultAddressFromInstance = nftxVaultFactory.try_vault(vaultId);
-  if (vaultAddressFromInstance.reverted) return ADDRESS_ZERO;
-  return vaultAddressFromInstance.value;
 }
 
 function getXTokenShareValue(vaultId: BigInt, network: string): BigInt {
-  const nftxInventoryStaking = NFTXInventoryStaking.bind(NFTX_INVENTORY_STAKING(network))
-  const xTokenShareValueFromInstance = nftxInventoryStaking.try_xTokenShareValue(vaultId);
+  const nftxInventoryStaking = NFTXInventoryStaking.bind(
+    NFTX_INVENTORY_STAKING(network)
+  );
+  const xTokenShareValueFromInstance = nftxInventoryStaking.try_xTokenShareValue(
+    vaultId
+  );
   if (xTokenShareValueFromInstance.reverted) return BigInt.fromI32(0);
   return xTokenShareValueFromInstance.value;
 }
--- src/mappings/utils/vaultIdAssignment.ts
+++ src/mappings/utils/vaultIdAssignment.ts
@@ -1,5 +1,5 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts";
-import { ERC20Contract } from "../../../generated/schema";
+import { ERC20Contract, VaultAddressLookup } from "../../../generated/schema";
 import { Token } from "../../../generated/templates";
 import { fetchAccount } from "./fetch/account";
 import { fetchVaultAsset } from "./fetch/vaultAsset";
@@ -11,11 +11,22 @@ export function createTokenAndAssignAssetInfo(
 ): void {
   const account = fetchAccount(address);
   const vaultAsset = fetchVaultAsset(account.id);
-  const tokenContract = ERC20Contract.load(account.id);
+  //const tokenContract = ERC20Contract.load(account.id);
 
-  if (!tokenContract) Token.create(address);
+  Token.create(address);
 
   vaultAsset.vaultId = vaultId;
   vaultAsset.type = type;
   vaultAsset.save();
+}
+
+
+export function createVaultAddressLookup(address: Address, vaultId: BigInt) : void {
+  let vaultAddressLookup = new VaultAddressLookup(vaultId.toHexString());
+  vaultAddressLookup.address = address;
+  vaultAddressLookup.save();
+}
+
+export function getVaultFromID(vaultId: BigInt) : VaultAddressLookup | null {
+  return VaultAddressLookup.load(vaultId.toHexString());
 }
